4f95445e8e59e8765c4fe7ed04afdf1071b8f1db
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-10 16:49:57 +0200

    initial array data created

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <main className="row py-4">
         <List />
       </main>
+      <Form />
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,9 @@
+const items = [
+  { id: 0, name: "Pan", price: 0.75 },
+  { id: 1, name: "Leche", price: 0.85 },
+  { id: 2, name: "Azucar", price: 0.55 },
+  { id: 3, name: "Harina", price: 0.8 },
+  { id: 4, name: "Queso", price: 3.25 },
+];
+
+export default items;
